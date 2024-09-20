044269f179742e768c9bffd6f1d8b9f0f4029d2f
JigyashaSharma <168462452+JigyashaSharma@users.noreply.github.com>
2024-09-20 12:30:19 +1200

    Week1 -Task2
    Extend the ShapeColourHelper static class so that it can make a shape a random colour from Yellow, Red, Blue, Green or Orange.

--- Classes/ShapeColourHelper.cs
+++ Classes/ShapeColourHelper.cs
@@ -20,5 +20,20 @@ namespace Week1ObjectOriented.Classes
             return s;
         }
 
+        //2. Extend the ShapeColourHelper static class so that it can make a shape a random colour from Yellow, Red, Blue, Green or Orange.
+        // using random class
+        public static void RandomColour(Shape s)
+        {
+            string[] colourList = { "Yellow", "Red", "Blue", "Green", "Orange" };
+
+            Random rand = new Random();
+            string? oldColour = s.Colour;
+
+            //Making sure it selects some new colour to differentiate
+            while (s.Colour == oldColour)
+            {
+                s.Colour = colourList[rand.Next(colourList.Length)];
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -37,6 +37,13 @@ ShapeColourHelper.ChangeColour(square);
 
 Console.WriteLine(square.Colour);
 
+//2. Checking the RandomColour methods of ShapeColourHelper
+Console.WriteLine("Old Sqaure color is: " + square.Colour);
+ShapeColourHelper.RandomColour(square);
+Console.WriteLine("New Sqaure color after RandomColour is: " + square.Colour);
+ShapeColourHelper.RandomColour(square);
+Console.WriteLine("New Sqaure color after RandomColour is: " + square.Colour);
+
 Console.ReadLine();
 
 
